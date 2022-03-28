9c8fb4eef21a6494c3bff8da3aea9ce6986211b9
Kavita Laddad <kavita.laddad@gmail.com>
2022-03-27 19:58:41 -0700

    Make upload roles configurable

--- src/authHelper.ts
+++ src/authHelper.ts
@@ -2,12 +2,14 @@ import { Request, Response, NextFunction } from "express";
 import * as jwt from "jsonwebtoken";
 
 export default class AuthHelper {
+  static uploadRoles = process.env.UPLOAD_ROLES.split(",").map((role) => role.trim());
+
   static checkJwt(req: Request, res: Response, next: NextFunction) {
     try {
       const token = req.headers.authorization.split(" ")[1] as string;
       let jwtPayload = jwt.verify(token, process.env.JWT_SECRET) as any;
       res.locals.jwtPayload = jwtPayload;
-      if (jwtPayload.role === "ADMIN") {
+      if (AuthHelper.uploadRoles.indexOf(jwtPayload.role) !== -1) {
         next();
       } else {
         res.status(401).send({ message: "Unauthorized" });
